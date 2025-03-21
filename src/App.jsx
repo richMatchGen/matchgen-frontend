9fbe3cef20a613ba7e46ed4eb5a35736fb11bb5a
richMatchGen <rich@matchgen.co.uk>
2025-03-21 21:02:22 +0000

    registernotsignup

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} /> {/* ✅ New route */}
+      <Route path="/register" element={<Signup />} />
       <Route path="/dashboard" element={<h1>Dashboard (Protected)</h1>} />
 
     </Routes>
