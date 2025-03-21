b3778b03553699d9a5fd89611a98704733e4b2e0
richMatchGen <rich@matchgen.co.uk>
2025-03-21 20:43:13 +0000

    reactduplicate

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import ReactDOM from 'react-dom/client';
-import './index.css'
-import App from './App.jsx'
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-// import theme from "./themes";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import AppTheme from './themes/AppTheme';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppTheme>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </AppTheme>
-  </StrictMode>,
-)
+  </StrictMode>
+);
