f7a31d0ede0298ef34f59c19bc79f8162c8d5d21
richMatchGen <rich@matchgen.co.uk>
2025-03-21 20:47:44 +0000

    appjsxcleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useEffect, useState } from "react";
-import { fetchMatches } from "./api";
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<h1>Dashboard (Protected)</h1>} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<h1>Dashboard (Protected)</h1>} />
+    </Routes>
   );
 }
 
