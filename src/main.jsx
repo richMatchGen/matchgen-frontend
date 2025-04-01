ff11a85b777cde99e1796458b9b2cd8bea2b7dcb
richMatchGen <rich@matchgen.co.uk>
2025-04-01 11:14:49 +0100

    mainCSS

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import AppTheme from './themes/AppTheme';
 import { BrowserRouter } from 'react-router-dom';
 
