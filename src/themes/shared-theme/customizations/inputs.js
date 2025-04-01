0921b857f6d3c57fd77bf4e345568de4b67ad2e3
richMatchGen <rich@matchgen.co.uk>
2025-04-01 13:39:24 +0100

    checkbox

--- src/themes/shared-theme/customizations/inputs.js
+++ src/themes/shared-theme/customizations/inputs.js
@@ -312,10 +312,8 @@ export const inputsCustomizations = {
   MuiCheckbox: {
     defaultProps: {
       disableRipple: true,
-      icon: (
-        <CheckBoxOutlineBlankRoundedIcon sx={{ color: 'hsla(210, 0%, 0%, 0.0)' }} />
-      ),
-      checkedIcon: <CheckRoundedIcon sx={{ height: 14, width: 14 }} />,
+      icon: () => <CheckBoxOutlineBlankRoundedIcon sx={{ color: 'hsla(210, 0%, 0%, 0.0)' }} />,
+      checkedIcon: () => <CheckRoundedIcon sx={{ height: 14, width: 14 }} />,
       indeterminateIcon: <RemoveRoundedIcon sx={{ height: 14, width: 14 }} />,
     },
     styleOverrides: {
