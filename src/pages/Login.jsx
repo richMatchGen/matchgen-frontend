282cf6a63da81ec0f5a1c5136003ad67a479bdd3
richMatchGen <rich@matchgen.co.uk>
2025-04-01 10:14:00 +0100

    appTheme

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,24 @@ import React, { useState } from "react";
 import { TextField, Button, Container, Typography } from "@mui/material";
 import axios from "axios";
 import useAuth from "../hooks/useAuth";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Divider from '@mui/material/Divider';
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import MuiCard from '@mui/material/Card';
+import { styled } from '@mui/material/styles';
+import ForgotPassword from './components/ForgotPassword';
+import AppTheme from './theme/AppTheme';
+import ColorModeSelect from './theme/ColorModeSelect';
+import { GoogleIcon, FacebookIcon, SitemarkIcon } from './components/CustomIcons';
 
 const Login = () => {
   const [email, setEmail] = useState("");
