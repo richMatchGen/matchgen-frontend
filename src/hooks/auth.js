b464f6f80575fb52f584c9b003199ff2a18e583c
richMatchGen <rich@matchgen.co.uk>
2025-04-02 14:41:41 +0100

    backendAPIURL

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://matchgen-api.up.railway.app/api/users/";
+const API_URL = "https://matchgen-backend-production.up.railway.app/api/users/";
 
 export const login = async (username, password) => {
   try {
