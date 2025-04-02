fa1b34fbb3efc043f9897babf9f10f67dc40668f
richMatchGen <rich@matchgen.co.uk>
2025-04-02 11:14:34 +0100

    HighlightCards

--- src/components/MainGrid.jsx
+++ src/components/MainGrid.jsx
@@ -64,6 +64,15 @@ export default function MainGrid() {
             <StatCard {...card} />
           </Grid>
         ))} */}
+                <Grid size={{ xs: 12, sm: 6, lg: 3 }}>
+          <HighlightedCard />
+        </Grid>
+        <Grid size={{ xs: 12, sm: 6, lg: 3 }}>
+          <HighlightedCard />
+        </Grid>
+                <Grid size={{ xs: 12, sm: 6, lg: 3 }}>
+          <HighlightedCard />
+        </Grid>
         <Grid size={{ xs: 12, sm: 6, lg: 3 }}>
           <HighlightedCard />
         </Grid>
