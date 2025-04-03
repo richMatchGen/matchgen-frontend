841a42a93ff33f35bfa872b53b5729d709776299
richMatchGen <rich@matchgen.co.uk>
2025-04-03 20:49:14 +0100

    mdexamplesdirect

--- src/examples/Items/NotificationItem/index.jsx
+++ src/examples/Items/NotificationItem/index.jsx
@@ -23,21 +23,21 @@ import MenuItem from "@mui/material/MenuItem";
 import Link from "@mui/material/Link";
 
 // Material Dashboard 2 React components
-import MDBox from "components/MDBox";
-import MDTypography from "components/MDTypography";
+import MDBox from "../../../components/MDBox";
+import Typography from "../../../components/Typography";
 
 // custom styles for the NotificationItem
-import menuItem from "examples/Items/NotificationItem/styles";
+import menuItem from "../../../examples/Items/NotificationItem/styles";
 
 const NotificationItem = forwardRef(({ icon, title, ...rest }, ref) => (
   <MenuItem {...rest} ref={ref} sx={(theme) => menuItem(theme)}>
     <MDBox component={Link} py={0.5} display="flex" alignItems="center" lineHeight={1}>
-      <MDTypography variant="body1" color="secondary" lineHeight={0.75}>
+      <Typography variant="body1" color="secondary" lineHeight={0.75}>
         {icon}
-      </MDTypography>
-      <MDTypography variant="button" fontWeight="regular" sx={{ ml: 1 }}>
+      </Typography>
+      <Typography variant="button" fontWeight="regular" sx={{ ml: 1 }}>
         {title}
-      </MDTypography>
+      </Typography>
     </MDBox>
   </MenuItem>
 ));
