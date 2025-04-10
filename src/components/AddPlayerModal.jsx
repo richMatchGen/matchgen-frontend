837a401ed01ab139d7002ccd71709590e4450a8e
richMatchGen <rich@matchgen.co.uk>
2025-04-10 10:14:21 +0100

    addplayerModalForm

--- src/components/AddPlayerModal.jsx
+++ src/components/AddPlayerModal.jsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
+import { addPlayer } from "../hooks/club";
+import { getToken } from "../hooks/auth";
 
 const style = {
   position: 'absolute',
@@ -10,7 +12,7 @@ const style = {
   transform: 'translate(-50%, -50%)',
   width: 400,
   bgcolor: 'background.paper',
-  border: '2px solid #000',
+  border: '0px solid #000',
   boxShadow: 24,
   pt: 2,
   px: 4,
@@ -49,6 +51,14 @@ function ChildModal() {
 
 export default function NestedModal() {
   const [open, setOpen] = React.useState(false);
+  const [newPlayer, setNewPlayer] = useState({ name: "", position: "" });
+  const token = getToken();
+
+  const handleAddPlayer = async () => {
+      await addPlayer(token, newPlayer);
+      setNewPlayer({ name: "", position: "" });
+    };
+
   const handleOpen = () => {
     setOpen(true);
   };
@@ -66,10 +76,13 @@ export default function NestedModal() {
         aria-describedby="parent-modal-description"
       >
         <Box sx={{ ...style, width: 400 }}>
-          <h2 id="parent-modal-title">Text in a modal</h2>
-          <p id="parent-modal-description">
+          <h2 id="parent-modal-title">Add Player</h2>
+          {/* <p id="parent-modal-description">
             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </p>
+          </p> */}
+      <input placeholder="Player Name" class="input input-bordered" value={newPlayer.name} onChange={e => setNewPlayer({ ...newPlayer, name: e.target.value })} />
+      <input placeholder="Position" class="input input-bordered" value={newPlayer.position} onChange={e => setNewPlayer({ ...newPlayer, position: e.target.value })} />
+      <button className="btn btn-primary" onClick={handleAddPlayer}>Add Player</button>
           <ChildModal />
         </Box>
       </Modal>
