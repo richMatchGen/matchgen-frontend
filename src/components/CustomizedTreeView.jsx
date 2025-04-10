971b6db0dbc43367bbf53b8c1060d5e02977aa2a
richMatchGen <rich@matchgen.co.uk>
2025-04-10 12:58:34 +0100

    listNestedModal

--- src/components/CustomizedTreeView.jsx
+++ src/components/CustomizedTreeView.jsx
@@ -27,7 +27,12 @@ const ITEMS = [
     id: '1',
     label: 'Squad, Results & Fixtures',
     children: [
-      { id: '1.1', label: 'Open Modal', component: <NestedModal /> },
+      {
+        id: '1.1',
+        label: 'Create Player',
+        color: 'green',
+        modal: 'NestedModal' // or just `id`, whatever you want to trigger it by
+      },      
       { id: '1.2', label: 'Create Player', color: 'green' },
       { id: '1.3', label: 'Enter Result', color: 'green' },
       { id: '1.4', label: 'Add Fixture', color: 'green' },
