24daf419db9eb7b31824e67f637fc0d141506651
richMatchGen <rich@matchgen.co.uk>
2025-04-10 17:02:49 +0100

    bgImage

--- src/components/AppNavBar.jsx
+++ src/components/AppNavBar.jsx
@@ -93,12 +93,12 @@ export function CustomIcon() {
         justifyContent: 'center',
         alignItems: 'center',
         alignSelf: 'center',
-        backgroundImage:
-          'linear-gradient(135deg, hsl(210, 98%, 60%) 0%, hsl(210, 100%, 35%) 100%)',
-        color: 'hsla(210, 100%, 95%, 0.9)',
-        border: '1px solid',
-        borderColor: 'hsl(210, 100%, 55%)',
-        boxShadow: 'inset 0 2px 5px rgba(255, 255, 255, 0.3)',
+        // backgroundImage:
+        //   'linear-gradient(135deg, hsl(210, 98%, 60%) 0%, hsl(210, 100%, 35%) 100%)',
+        // color: 'hsla(210, 100%, 95%, 0.9)',
+        // border: '1px solid',
+        // borderColor: 'hsl(210, 100%, 55%)',
+        // boxShadow: 'inset 0 2px 5px rgba(255, 255, 255, 0.3)',
       }}
     >
       {/* <SitemarkIcon /> */}
