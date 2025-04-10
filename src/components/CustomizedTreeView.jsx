1e4e1f308587f1f81a1a721779cae6b8d76cacf0
richMatchGen <rich@matchgen.co.uk>
2025-04-10 12:44:51 +0100

    nestedModalwithinList

--- src/components/CustomizedTreeView.jsx
+++ src/components/CustomizedTreeView.jsx
@@ -27,10 +27,13 @@ const ITEMS = [
     id: '1',
     label: 'Squad, Results & Fixtures',
     children: [
-      <NestedModal />,
-      { id: '1.1', label: 'Create Player', color: 'green' },
-      { id: '1.2', label: 'Enter Result', color: 'green' },
-      { id: '1.3', label: 'Add Fixture', color: 'green' },
+      {
+        id: '1.1',
+        label: <NestedModal />, // Assuming this is a component
+      },
+      { id: '1.2', label: 'Create Player', color: 'green' },
+      { id: '1.3', label: 'Enter Result', color: 'green' },
+      { id: '1.4', label: 'Add Fixture', color: 'green' },
       // {
       //   id: '1.4',
       //   label: 'Blog',
