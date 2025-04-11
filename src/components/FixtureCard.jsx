5138082a17267f27a213b701e7699a94dacc3aab
richMatchGen <rich@matchgen.co.uk>
2025-04-11 13:28:31 +0100

    boximport

--- src/components/FixtureCard.jsx
+++ src/components/FixtureCard.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
 import SportsIcon from '@mui/icons-material/Sports';
