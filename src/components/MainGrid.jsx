46b24d6e5d1b06c7c59c0bae21b97a00b10b9118
richMatchGen <rich@matchgen.co.uk>
2025-08-27 18:52:10 +0100

    Fix missing Box import in ResultCard component - Dashboard was failing due to undefined Box

--- src/components/MainGrid.jsx
+++ src/components/MainGrid.jsx
@@ -50,7 +50,7 @@ export default function MainGrid() {
         container
         spacing={2}
         columns={12}
-        sx={{ mb: (AppTheme) => AppTheme.spacing(2) }}
+        sx={{ mb: 2 }}
       >
         {/* {data.map((card, index) => (
           <Grid key={index} size={{ xs: 12, sm: 6, lg: 3 }}>
